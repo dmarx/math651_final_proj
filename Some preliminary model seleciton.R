8922440d16074dbd4651ad28611c26fefe8732ed
David Marx <david.marx84@gmail.com>
2013-12-10 22:38:03 -0500

    Outlier analysis: full model, full + interactions. Working through full + selected interactions and selected univariate model.

--- Some preliminary model seleciton.R
+++ Some preliminary model seleciton.R
@@ -143,3 +143,289 @@ dat[,-c("a1")]
 
 1 in c(1,2)
 
+####################################################3
+
+# Unconstrained full first-order model with bivariate interactions
+# Focusing on algae a6
+
+names(dat)[1:11]
+# Dynamically build up formula
+formula1 = 'a6 ~ '
+formula2 = 'a6 ~ '
+for(n in names(dat)[1:11]){
+  formula1 = paste(formula1, n, '+')
+  formula2 = paste(formula2, n, '+') # full model, no interactions
+  for(n2 in names(dat)[1:11]){
+    if(n < n2){ # alphabetic deduplication
+      interaction = paste(n,':',n2, sep="")
+      formula1 = paste(formula1, interaction, '+')
+    }
+  }
+}
+
+# Get rid of trailing plus sign
+formula.full.interact = substr(formula1, 1, nchar(formula1)-2)
+formula.full = substr(formula2, 1, nchar(formula2)-2)
+
+mod.full          = lm(formula.full, dat)          # adj.rsq = .355
+mod.full.interact = lm(formula.full.interact, dat) # adj.rsq = .575
+
+# Step regression to trim out weakest interactions from full model
+mod.full.selectInteract = step(mod.full.interact
+                              ,scope=list(end=formula.full, start=formula.full.interact)
+                              ,direction='backward')
+
+# Full model with select interactions
+# --> all univariate terms kept in model to ensure that output of backwards selection 
+#     on interactions model gives valid model. Otherwise, result is all interactions.
+summary(mod.full)
+summary(mod.full.selectInteract) # .664
+
+
+# Call:
+# lm(formula = a6 ~ season + size + speed + mxPH + mnO2 + Cl + 
+# NO3 + NH4 + oPO4 + PO4 + Chla + season:speed + speed:mxPH + 
+# mxPH:NO3 + season:mnO2 + size:mnO2 + speed:mnO2 + mnO2:NO3 + 
+# mnO2:NH4 + mnO2:oPO4 + size:Cl + speed:Cl + mxPH:Cl + Cl:NH4 + 
+# Cl:PO4 + NO3:oPO4 + season:NH4 + size:NH4 + speed:NH4 + NO3:NH4 + 
+# NH4:oPO4 + season:oPO4 + speed:oPO4 + oPO4:PO4 + season:PO4 + 
+# size:PO4 + speed:PO4 + size:Chla + mxPH:Chla + mnO2:Chla + 
+# oPO4:Chla, data = dat)
+
+# Residuals:
+# Min       1Q   Median       3Q      Max 
+# -12.4749  -3.1048  -0.2255   2.5452  22.7681 
+
+# Coefficients:
+# Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)              -2.397e+01  2.646e+01  -0.906 0.366644    
+# seasonspring             -6.493e+00  1.588e+01  -0.409 0.683274    
+# seasonsummer             -1.798e+01  1.554e+01  -1.157 0.249269    
+# seasonwinter             -2.889e+01  1.466e+01  -1.970 0.050936 .  
+# sizemedium                3.471e+01  1.048e+01   3.313 0.001195 ** 
+# sizesmall                 3.979e+01  1.254e+01   3.174 0.001875 ** 
+# speedlow                  1.265e+02  5.122e+01   2.469 0.014847 *  
+# speedmedium              -4.643e+01  2.092e+01  -2.219 0.028197 *  
+# mxPH                      3.155e-01  2.425e+00   0.130 0.896667    
+# mnO2                      1.899e+00  1.611e+00   1.179 0.240693    
+# Cl                       -5.220e-01  3.781e-01  -1.381 0.169706    
+# NO3                       8.743e+00  5.907e+00   1.480 0.141261    
+# NH4                       4.098e-02  1.618e-02   2.533 0.012491 *  
+# oPO4                     -2.498e-01  1.118e-01  -2.235 0.027096 *  
+# PO4                       2.652e-01  5.384e-02   4.926 2.51e-06 ***
+# Chla                     -1.000e+00  6.966e-01  -1.436 0.153439    
+# seasonspring:speedlow    -1.990e+01  8.747e+00  -2.275 0.024547 *  
+# seasonsummer:speedlow    -1.655e+01  6.489e+00  -2.550 0.011919 *  
+# seasonwinter:speedlow    -6.017e+00  4.984e+00  -1.207 0.229571    
+# seasonspring:speedmedium  8.800e-01  4.312e+00   0.204 0.838605    
+# seasonsummer:speedmedium  3.014e+00  3.929e+00   0.767 0.444288    
+# seasonwinter:speedmedium  1.335e+01  4.000e+00   3.339 0.001098 ** 
+# speedlow:mxPH            -1.035e+01  5.646e+00  -1.832 0.069220 .  
+# speedmedium:mxPH          3.627e+00  2.211e+00   1.640 0.103378    
+# mxPH:NO3                 -1.524e+00  7.534e-01  -2.022 0.045192 *  
+# seasonspring:mnO2         6.431e-01  1.390e+00   0.463 0.644290    
+# seasonsummer:mnO2         1.909e+00  1.366e+00   1.397 0.164696    
+# seasonwinter:mnO2         2.523e+00  1.261e+00   2.000 0.047533 *  
+# sizemedium:mnO2          -3.088e+00  9.350e-01  -3.302 0.001239 ** 
+# sizesmall:mnO2           -3.668e+00  1.102e+00  -3.328 0.001139 ** 
+# speedlow:mnO2            -3.173e+00  1.258e+00  -2.522 0.012888 *  
+# speedmedium:mnO2          1.546e+00  8.650e-01   1.787 0.076302 .  
+# mnO2:NO3                  2.940e-01  1.507e-01   1.951 0.053193 .  
+# mnO2:NH4                 -4.187e-03  1.153e-03  -3.630 0.000406 ***
+# mnO2:oPO4                 8.314e-03  6.558e-03   1.268 0.207161    
+# sizemedium:Cl             8.630e-02  5.192e-02   1.662 0.098900 .  
+# sizesmall:Cl              1.216e-01  8.325e-02   1.461 0.146443    
+# speedlow:Cl              -6.484e-02  1.230e-01  -0.527 0.598898    
+# speedmedium:Cl            1.078e-01  7.363e-02   1.464 0.145660    
+# mxPH:Cl                   6.049e-02  4.472e-02   1.353 0.178473    
+# Cl:NH4                    1.926e-04  3.301e-05   5.836 4.04e-08 ***
+# Cl:PO4                   -7.623e-04  1.947e-04  -3.914 0.000146 ***
+# NO3:oPO4                  1.447e-02  5.789e-03   2.499 0.013700 *  
+# seasonspring:NH4         -1.450e-02  5.820e-03  -2.492 0.013977 *  
+# seasonsummer:NH4         -7.518e-03  3.240e-03  -2.320 0.021893 *  
+# seasonwinter:NH4         -6.715e-03  3.485e-03  -1.927 0.056208 .  
+# sizemedium:NH4           -1.370e-02  7.940e-03  -1.726 0.086806 .  
+# sizesmall:NH4            -1.759e-03  8.281e-03  -0.212 0.832105    
+# speedlow:NH4             -1.693e-02  9.846e-03  -1.719 0.088010 .  
+# speedmedium:NH4          -6.666e-03  6.962e-03  -0.957 0.340141    
+# NO3:NH4                   3.508e-04  1.633e-04   2.148 0.033556 *  
+# NH4:oPO4                 -3.128e-05  1.309e-05  -2.390 0.018289 *  
+# seasonspring:oPO4         3.389e-02  7.118e-02   0.476 0.634787    
+# seasonsummer:oPO4        -2.213e-01  5.776e-02  -3.831 0.000198 ***
+# seasonwinter:oPO4        -2.938e-02  5.932e-02  -0.495 0.621224    
+# speedlow:oPO4             2.150e-01  6.444e-02   3.336 0.001108 ** 
+# speedmedium:oPO4          8.682e-02  5.417e-02   1.603 0.111452    
+# oPO4:PO4                  1.728e-04  9.094e-05   1.900 0.059618 .  
+# seasonspring:PO4         -5.793e-02  4.815e-02  -1.203 0.231120    
+# seasonsummer:PO4          1.411e-01  3.649e-02   3.867 0.000173 ***
+# seasonwinter:PO4         -1.137e-02  4.113e-02  -0.276 0.782660    
+# sizemedium:PO4           -7.630e-02  3.297e-02  -2.314 0.022212 *  
+# sizesmall:PO4            -1.292e-01  3.646e-02  -3.544 0.000548 ***
+# speedlow:PO4             -1.399e-01  4.894e-02  -2.859 0.004948 ** 
+# speedmedium:PO4          -8.215e-02  4.186e-02  -1.963 0.051811 .  
+# sizemedium:Chla          -1.291e-01  1.005e-01  -1.284 0.201412    
+# sizesmall:Chla            1.851e-01  1.517e-01   1.220 0.224564    
+# mxPH:Chla                 1.773e-01  7.712e-02   2.299 0.023111 *  
+# mnO2:Chla                -4.371e-02  1.961e-02  -2.228 0.027572 *  
+# oPO4:Chla                -2.397e-03  8.074e-04  -2.968 0.003566 ** 
+# ---
+# Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1 
+
+# Residual standard error: 6.724 on 130 degrees of freedom
+# Multiple R-squared: 0.7828,  Adjusted R-squared: 0.6674 
+# F-statistic: 6.788 on 69 and 130 DF,  p-value: < 2.2e-16 
+
+############################################################################
+# Season is not significant by itself, but has some very significant interactions with 
+# PO4 and oPO4, in particular the summer factor. Similar situation with the Cl predictor.
+
+# Let's use the full model without interactions to find outliers, and repeat the analysis
+# with interactions and compare results.
+
+####################################33
+
+# Studentized deleted residuals
+del.res.full = studres(mod.full)
+del.res.full.interact = studres(mod.full.interact) # NaNs produced???
+
+sum(is.na(del.res.full.interact)) # 1 Na
+which(is.na(del.res.full.interact)) # 48
+dat[48,] # a6 = 0
+summary(mod.full.interact)$residual[48] # -5e-15, effectively 0
+min(abs(summary(mod.full.interact)$residual)) # Looks like this is the smallest absolute residual
+length(which(dat$a6==0)) # a6 is zero for 107 points. Why is THIS residual NA? Because it's close to zero, I guess...
+
+del.res.full.interact[48]=0
+
+
+# Bonferroni test
+n = nrow(dat)
+p = summary(mod.full.interact)$df[1]
+a = 0.01 
+crit.b = qt(1 - a/(2 * n), n - p - 1)  # 4.279
+max(abs(del.res.full.interact))  # 4.964
+which(del.res.full.interact>crit.b) # only observation 73
+
+# X outliers from leverage
+H.int = hatvalues(mod.full.interact)
+h.bar.int = p/n #.58
+which(H.int > 2 * h.bar.int)
+plot(sort(H.int))
+hist(H.int)
+# H takes values from 0 up to 1, pretty unifromly distributed
+
+crit.vals.full.int = list(cooks = 4/n
+                      , dffits = 2 * sqrt(p/n)
+                      , levg=2*h.bar.int
+                      , bonf=crit.b
+                      , dfbetas = 2/sqrt(n)
+)
+
+
+# repeat bonferroni test for model without interactions
+n = nrow(dat)
+p = summary(mod.full)$df[1]
+a = 0.01 
+crit.b = qt(1 - a/(2 * n), n - p - 1)  # 4.154
+max(abs(del.res.full))  # 5.938
+which(del.res.full>crit) # only observation 133, 170
+
+
+# X outliers from leverage
+H.full = hatvalues(mod.full)
+h.bar.full = p/n #.58
+H.full[which(H.full > 2 * h.bar.full) ] # crit = 0.16
+# 20        21        35        88        89        127       128       134       153 
+# 0.3309145 0.1609968 0.2072736 0.1689092 0.2429690 0.1753647 0.1974960 0.3630732 0.8931062
+H.full[133]
+plot(sort(H.full))
+hist(H.full)
+plot(mod.full, which=5)
+abline(v=.22, lty=2)
+abline(v=.4, lty=2)
+abline(v=2*h.bar.full, lty=3) 
+
+crit.vals.full = list(cooks = 4/n
+                      , dffits = 2 * sqrt(p/n)
+                      , levg=2*h.bar.full
+                      , bonf=crit.b
+                      , dfbetas = 2/sqrt(n)
+                      )
+
+# I think most of these aren't outliers by leverage. 
+# 153 is outlier by leverage. Leverages less than .4 seem close enough to main cluster for me. 
+# Maybe could go up to h=.22
+##
+# By cooks distance, 170, 133, and 134. 133 and 170 were outliers by studentized deleted
+# residual as well, we should probably get rid of these two, as well as 153.
+
+###########################################################################
+
+# narrowing in on 11 most problematic observations for additional analysis
+
+ix = c(20,21,35,73, 88,89,127,128,133,134,153,170)
+
+df.diag.full = data.frame(index = ix
+                          ,cooks = cooks.distance(mod.full)[ix]
+                          ,dffits = dffits(mod.full)[ix]
+                          ,leverage = H.full[ix]
+                          ,abs.stud.del.res = abs(del.res.full[ix]))
+
+df.eval.full = as.data.frame(abs(df.diag.full[,2:5]) > crit.vals.full[-5])
+df.eval.full = cbind(df.diag.full$index, df.eval.full)
+
+# Cooks and DFFITS coorborates influence of observations 89, 133, 134, 153, 170
+# NB cooks distance heuristic critical threshhold is TINY! Just .02. Ramping the
+# threshhold up to .2 narrows in on observations 133, 134, and 170, the points 
+# suggested by the residuals vs leverage plot of the full model without interactions.
+
+############################################################################
+
+# Repeat analysis for model with interaction terms.
+
+
+ix = c(20,21,35,73, 88,89,127,128,133,134,153,170)
+
+df.diag.full.int = data.frame(index = ix
+                          ,cooks = cooks.distance(mod.full.interact)[ix]
+                          ,dffits = dffits(mod.full.interact)[ix]
+                          ,leverage = H.int[ix]
+                          ,abs.stud.del.res = abs(del.res.full.interact[ix]))
+
+df.eval.full.int = as.data.frame(abs(df.diag.full.int[,2:5]) > crit.vals.full.int[-5])
+df.eval.full.int = cbind(df.diag.full$index, df.eval.full.int)
+
+# In the model with interaction terms, only observation 73 was highlighted as a problematic
+# point based on the deleted residuals, but it looks like we actually have many influential 
+# observations by cook's distance and DFFITS, including observation 73.
+# This analysis corroborates the influence of 89, 133, 134, 153, 170 observed above.
+#
+# Oddities:
+# Very high cook's distance observed in observations 153, 88, and 133. 
+# Very high |DFFITS| in observations 153, 88, 20, 133
+#
+# In addition to the dropping terms 133, 134, and 170, I also recommend we drop observations
+# 88 and 153 to mitigate influence in models including interaction terms.
+
+############################################################################
+
+# Repeat analysis for full model with select interaction terms. Cause why not.
+
+p.sel = summary(mod.full.selectInteract)$df[1]
+H.sel = hatvalues(mod.full.selectInteract)
+h.bar.sel = p.sel/n
+sum(H.sel > 2*h.bar.sel) # 21 observations... wowza. WTF?
+plot(sort(H.sel))
+abline(h=h.bar.sel, lty=2) # this is way too liberal of a cut off. 
+abline(h=.62, lty=2) # 0.62 is a way better cutoff
+sum(H.sel > .62) # 28 observations by this criterion. 
+sort(H.sel, decreasing=T)[1:28]
+# The usual suspects appear at the top of the list:
+# 153, 20, 133, 88, (35), 89, (34), 134, (...), 170.
+# Going down to obs 170 gives 15 observations to investigate by leverage.
+
+# Crazy how much the leverages change with each model
+# Clearly leverage is very, very model specific, especially in elevated dimensions.
+plot(H.full)
+points(H.int, col='blue')
+points(H.sel, col='red')
